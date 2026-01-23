400cc633b7bc3a37c53411007af4bb669751bfa9
Benedikt von Blomberg <git@bvb.anonaddy.com>
2026-01-23 22:23:01 +0100

    cleanup flake

--- flake.nix
+++ flake.nix
@@ -11,25 +11,22 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        # 1. Package 'rdricpp' separately (since it's not in nixpkgs)
+        # package 'rdricpp' (since it's not in nixpkgs)
         rdricpp = pkgs.stdenv.mkDerivation {
           pname = "rdricpp";
           version = "master";
-
           src = pkgs.fetchzip {
             url = "https://codeberg.org/xieamoe/rdricpp/archive/main.tar.gz";
-            # STEP 1: Run 'nix build', get the real hash from the error, and replace it here:
             sha256 = "sha256-xEiEcz/XNIxgK2kiNCcuQi7RWQLqg7W3IFmbkJ4px9k="; 
           };
 
           nativeBuildInputs = [ pkgs.cmake ];
         };
 
-        # 2. Package 'gd-tools'
+        # package 'gd-tools' itself
         gd-tools = pkgs.stdenv.mkDerivation {
           pname = "gd-tools";
           version = "1.5";
-
           src = pkgs.fetchzip {
             url = "https://codeberg.org/hashirama/gd-tools/archive/main.tar.gz";
             sha256 = "sha256-2LwWxLNwg9GPZcz/k4rgvVUo181pKvAHhrLxYZH1O6M=";
@@ -37,7 +34,6 @@
 
           nativeBuildInputs = with pkgs; [ cmake pkg-config ];
           
-          # We provide all libraries here so CMake finds them
           buildInputs = with pkgs; [ 
             marisa 
             libcpr 
@@ -47,7 +43,7 @@
             catch2_3
           ];
 
-          # We force GUIX mode, which disables automatic downloading
+          # We force GUIX mode, which disables automatic downloading of dependencies
           cmakeFlags = [ "-DGUIX=ON" ];
         };
 
@@ -55,7 +51,7 @@
       {
         packages.default = gd-tools;
         
-        # Optional: Development shell if you want to hack on it later
+        # development shell if you want to hack on it
         devShells.default = pkgs.mkShell {
           inputsFrom = [ gd-tools ];
         };
